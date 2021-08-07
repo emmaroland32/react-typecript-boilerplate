7a5a0d01da813394859082dc5663e46176db3b08
Segun Iyanda <siyanda@aiicocapital.com>
2021-08-07 20:59:58 +0100

    Added support for tailwind, internalization

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@
 // import 'react-app-polyfill/ie11';
 // import 'react-app-polyfill/stable';
 import ReactDOM from 'react-dom';
-// import 'typeface-poppins';
-// import './i18n';
+import 'typeface-poppins';
+import './i18n';
 // import './react-chartjs-2-defaults';
-// import './styles/app-base.css';
-// import './styles/app-components.css';
-// import './styles/app-utilities.css';
+import './styles/app-base.css';
+import './styles/app-components.css';
+import './styles/app-utilities.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
